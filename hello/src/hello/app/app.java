c217dfadea02dc39f6a45df767ab56caf4d3df0d
Atul <Atul@LAPTOP-82A25FR8>
2020-05-03 05:31:59 +0530

    change

---
+++ hello/src/hello/app/app.java
@@ -0,0 +1,8 @@
+package hello.app;
+
+public class app {
+	public app() {
+		System.out.println("app");
+	}
+
+}
---
+++ hello/src/hello/main.java
@@ -0,0 +1,12 @@
+package hello;
+
+import hello.app.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		app obk = new app();
+	}
+
+}
